19210350d813facbbb2117f32002f1254c9916b8
ritchieforest <51833315+ritchieforest@users.noreply.github.com>
2024-05-02 18:55:24 -0300

    commit

---
+++ SqlServer/FindColAllTables.sql
@@ -0,0 +1,17 @@
+SELECT
+sysobjects.name AS table_name,
+syscolumns.name AS column_name,
+systypes.name AS datatype,
+syscolumns.LENGTH AS LENGTH
+FROM
+sysobjects
+INNER JOIN syscolumns ON sysobjects.id = syscolumns.id
+INNER JOIN systypes ON syscolumns.xtype = systypes.xtype
+WHERE
+(sysobjects.xtype = 'U')
+and (
+UPPER(syscolumns.name) like upper('%myFieldName%')
+)
+ORDER BY
+sysobjects.name,
+syscolumns.colid
